35376e86d35e6eeab2948caafad05a4bd543d5c5
Hammad Ul Hassan <hammadulhassanqureshi@gmail.com>
2023-03-25 12:38:15 +0500

    git commit --allow-empty -m "Empty-Commit"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Classprops from './Classprops';
+import Functionalprops from './Functionalprops';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div><Classprops name="Hammad" place="Rawalpindi">
+        <p>Child Component</p>
+      </Classprops>
+      <Classprops name="Hammad Ul Hassan" place="Rawalpindi">
+        <button>Click</button>
+      </Classprops>
+      <Classprops name="Hammad Ul Hassan Qureshi" place="Rawalpindi"/>
+      {/* <Functionalprops name= "Hammad Qureshi" place="Rawalpindi"/> {" "} */}
+      </div>
+      )
+  }
 }
 
 export default App;
---
+++ src/Functionalprops.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Functionalprops(props) {
+  return (
+    <div><h3>This is Functional Components</h3>
+    <h3>Welcome {this.props.name} from {this.props.name}</h3></div>
+  );
+}
+export default Functionalprops;
